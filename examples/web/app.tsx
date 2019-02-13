a310d8c588b2984867d2633aa865aad7e29294d0
Jason Abbott <dev@trailimage.com>
2019-02-13 14:33:22 -0700

    examples

--- examples/web/app.tsx
+++ examples/web/app.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 const frogID = require('../../src/__mocks__/logo-colored.svg') as string;
+const awardID = require('../../src/__mocks__/heroicon-award-lg.svg') as string;
 
 export class ExampleApp extends React.Component<any, any> {
    render() {
       return (
          <div>
             <h2>Example Application</h2>
-            <svg>
+            <svg viewBox="0 0 50 50">
                <use href={frogID} />
+               <use href={awardID} y={20} />
             </svg>
          </div>
       );
