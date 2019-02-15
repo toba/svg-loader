44a1f522b6c6791efdaa7e03956453f8a03c79df
Jason Abbott <dev@trailimage.com>
2019-02-14 20:50:58 -0700

    work on mobile

--- examples/web/app.tsx
+++ examples/web/app.tsx
@@ -1,15 +1,20 @@
+import '../../src/svg';
 import React from 'react';
-const frogID = require('../../src/__mocks__/logo-colored.svg') as string;
-const awardID = require('../../src/__mocks__/heroicon-award-lg.svg') as string;
+import frogID from '../../src/__mocks__/logo-colored.svg';
+import awardID from '../../src/__mocks__/heroicon-award-lg.svg';
 
 export class ExampleApp extends React.Component<any, any> {
    render() {
+      const style = {
+         width: '100px',
+         height: '200px'
+      };
       return (
          <div>
             <h2>Example Application</h2>
-            <svg viewBox="0 0 50 50">
-               <use href={frogID} />
-               <use href={awardID} y={20} />
+            <svg viewBox="0 0 100 200" style={style}>
+               <use href={frogID} height={100} width={100} y={0} />
+               <use href={awardID} height={100} y={100} />
             </svg>
          </div>
       );
