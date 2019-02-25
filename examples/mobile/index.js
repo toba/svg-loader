9a133eb08aea30a0a22196a96c7eb77ae67693c4
Jason Abbott <dev@trailimage.com>
2019-02-24 17:16:54 -0700

    merge back

--- examples/mobile/index.js
+++
@@ -1,5 +0,0 @@
-import { AppRegistry } from 'react-native';
-import { ExampleApp } from './example-app';
-import { name as appName } from './app.json';
-
-AppRegistry.registerComponent(appName, () => ExampleApp);
